c527ee17434a0cbeaf6d8507790a5040ea4e91de
Jet <jianquan.xie@infinitypath.com.au>
2022-12-14 09:51:40 +1000

    fish shell integration

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -2,3 +2,11 @@ function fish_user_key_bindings
   fish_vi_key_bindings
   bind -M insert -m default jk backward-char force-repaint
 end
+
+function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
+    if test "$argv" = !!
+    eval command sudo $history[1]
+else
+    command sudo $argv
+    end
+end
