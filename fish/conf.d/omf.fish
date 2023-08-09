3c06c11efe287b4b4c0837caa8c6d2bd697d0f29
Jet <jet.xie@partscheck.net.au>
2023-08-09 13:49:21 +1000

    fish fzf scripts

--- fish/conf.d/omf.fish
+++ fish/conf.d/omf.fish
@@ -10,8 +10,15 @@ alias c='clear'
 alias ls='lsd'
 alias bat='batcat'
 alias fv='fzf --print0 | xargs -0 -o nvim'
+alias sd "cd ~ && cd (find * -type d | fzf)"
 
 fish_vi_key_bindings
 
 set -gx FZF_DEFAULT_COMMAND "fdfind . $HOME"
+
+set -gx FZF_DEFAULT_OPTS "
+--preview 'batcat --style numbers,changes --theme Dracula --color=always  {}'
+--color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
+--color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
+"
 
--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,45 @@
 set fish_key_bindings fish_user_key_bindings
+
+function fh
+    set cmd (history | fzf --preview "echo {} | awk '{\$1=\"\"; print \$0}'")
+    if test -n "$cmd"
+        eval $cmd
+    end
+end
+
+function fk
+   set pid (ps -ef | sed 1d | fzf -m | awk '{print $2}')
+    if test -n "$pid"
+        set signal
+        if test -n "$argv[1]"
+            set signal "-$argv[1]"
+        else
+            set signal -9
+        end
+        echo $pid | xargs kill $signal
+    end
+end
+
+function fcd --description "Fuzzy change directory"
+    if set -q argv[1]
+        set searchdir $argv[1]
+    else
+        set searchdir $HOME
+    end
+
+    # https://github.com/fish-shell/fish-shell/issues/1362
+    set -l tmpfile (mktemp)
+    fdfind . $searchdir -t d | fzf > $tmpfile
+    set -l destdir (cat $tmpfile)
+    rm -f $tmpfile
+
+    if test -z "$destdir"
+        return 1
+    end
+
+    cd $destdir
+end
+
+
 starship init fish | source
 direnv hook fish | source
