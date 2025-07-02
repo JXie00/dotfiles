b4ef3b48dd6ed268dd652ea80c32fc343b38064e
Jet Xie <jet.xie@flightcentre.com>
2025-07-02 10:35:23 +1000

    neovim config update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -21,68 +21,89 @@ local map = vim.keymap.set
 --vim.keymap.set('n', 'h', 'n') -- Keep 'h' for next search match
 --vim.keymap.set('v', 'h', 'n') -- Same for visual mode
 
+--vim.keymap.del("n", "<leader>/")
 
-vim.keymap.del("n", "<leader>/")
-
-vim.keymap.set('i', 'jk', '<ESC>')
+vim.keymap.set("i", "jk", "<ESC>")
 vim.keymap.set("n", "<leader>wS", "<C-W>v", { desc = "Split Window Right", remap = true })
 
-vim.keymap.set("n", "<leader>tt", function() Snacks.terminal(nil, { cwd = LazyVim.root() }) end, { desc = "Terminal (Root Dir)" })
+vim.keymap.set("n", "<leader>tt", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
 
-vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
 map("t", "<C-t>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 
-vim.keymap.set('n', '<leader>k', '<cmd>bnext<cr>')
-vim.keymap.set('n', '<leader>j', '<cmd>bprevious<cr>')
+vim.keymap.set("n", "<leader>k", "<cmd>bnext<cr>")
+vim.keymap.set("n", "<leader>j", "<cmd>bprevious<cr>")
 
-vim.keymap.set('n', '<leader>?', "<cmd> Telescope oldfiles<cr>", { desc = '[?] Find recently opened files' })
+vim.keymap.set("n", "<leader>?", "<cmd>FzfLua oldfiles<cr>", { desc = "[?] Find recently opened files" })
 
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set(
+  "n",
+  "<leader><space>",
+  "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>",
+  { desc = "[ ] Find existing buffers" }
+)
 
-vim.keymap.set('n', '<leader>,', "<cmd> Telescope commands<cr>", { desc = '[ ] List available commands' })
+vim.keymap.set("n", "<leader>,", "<cmd>FzfLua commands<cr>", { desc = "[ ] List available commands", remap = true })
 
-vim.keymap.set('n', '<leader>/', require('telescope.builtin').current_buffer_fuzzy_find, { desc = '[ ] Current buffer fuzzy' })
+vim.keymap.set("n", "<leader>/", "<Cmd>FzfLua grep_curbuf<CR>", { desc = "[ ] Current buffer fuzzy", remap = true })
 
+vim.keymap.set(
+  "n",
+  "<leader>.",
+  LazyVim.pick("files", { root = false, cwd = vim.fn.expand("%:p:h") }),
+  { desc = "Find Files (cwd)", remap = true }
+)
 
+vim.keymap.set("n", "<leader>fh", "<cmd>FzfLua help_tags<cr>", { desc = "[S]earch [H]elp", remap = true })
 
-vim.keymap.set('n', '<leader>.',  LazyVim.pick("files", { root = false }), { desc = "Find Files (cwd)"})
+vim.keymap.set(
+  "n",
+  "<leader>fw",
+  LazyVim.pick("grep_string", { word_match = "-w" }),
+  { desc = "Word (Root Dir)", remap = true }
+)
+vim.keymap.set("n", "<leader>fg", LazyVim.pick("live_grep"), { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader>fG", LazyVim.pick("live_grep", { root = false }), { desc = "[S]earch by [G]rep (CWD)" })
 
+vim.keymap.set("n", "<leader>fd", "<cmd>FzfLua diagnostics_document<cr>", { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set(
+  "n",
+  "<leader>fD",
+  "<cmd>FzfLua diagnostics_workspace<cr>",
+  { desc = "[S]earch [D]iagnostics workspace" }
+)
+vim.keymap.set("n", "<leader>fr", "<cmd>FzfLua registers<cr>", { desc = "[S]earch [R]egesters" })
+vim.keymap.set("n", "<leader>fm", "<cmd>FzfLua marks<cr>", { desc = "[S]earch [M]arks" })
 
-vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set("n", "<leader>ch", "<cmd>FzfLua command_history<cr>", { desc = "[S]earch Command history" })
+vim.keymap.set(
+  "n",
+  "<leader>gc",
+  "<cmd>FzfLua git_bcommits<cr>",
+  { desc = "Lists buffers git commits with diff preview and checks them out on <cr>" }
+)
+vim.keymap.set("n", "<leader>gC", "<cmd>FzfLua git_commits<cr>", { desc = "lists git commits with diff preview, " })
+vim.keymap.set("n", "<leader>gb", "<cmd>FzfLua git_branches<cr>", { desc = "List all branches with diff view" })
+vim.keymap.set(
+  "n",
+  "<leader>go",
+  "<cmd>FzfLua git_status<cr>",
+  { desc = "Lists current changes per file with diff preview and add action" }
+)
 
-vim.keymap.set('n', '<leader>fw', LazyVim.pick("grep_string", { word_match = "-w" }), {desc = "Word (Root Dir)"})
-vim.keymap.set('n', '<leader>fg', LazyVim.pick("live_grep"), { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>fG', LazyVim.pick("live_grep",{root = false}), { desc = '[S]earch by [G]rep (CWD)' })
+map("n", "gi", vim.lsp.buf.implementation, { desc = "go to implementation" })
 
-
-vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>fr', require('telescope.builtin').registers, { desc = '[S]earch [R]egesters' })
-vim.keymap.set('n', '<leader>fm', require('telescope.builtin').marks, { desc = '[S]earch [M]arks' })
-
-
-vim.keymap.set('n', '<leader>ch', require('telescope.builtin').command_history, { desc = '[S]earch Command history' })
-vim.keymap.set('n', '<leader>gc', require('telescope.builtin').git_bcommits,
-  { desc = 'Lists buffers git commits with diff preview and checks them out on <cr>' })
-vim.keymap.set('n', '<leader>gC', require('telescope.builtin').git_commits,
-  { desc = 'lists git commits with diff preview, ' })
-vim.keymap.set('n', '<leader>gb', require('telescope.builtin').git_branches,
-  { desc = 'List all branches with diff view' })
-vim.keymap.set('n', '<leader>go', require('telescope.builtin').git_status,
-  { desc = 'Lists current changes per file with diff preview and add action' })
-
-map('n', "gi", vim.lsp.buf.implementation, {desc = "go to implementation"})
-
-map('n','su', require('telescope.builtin').lsp_references, {desc = '[G]oto [R]eferences'})
-map('n','fo', require('telescope.builtin').lsp_document_symbols, {desc = '[D]ocument [S]ymbols'})
-map('n','<leader>fo', require('telescope.builtin').lsp_dynamic_workspace_symbols, {desc = '[W]orkspace [S]ymbols'})
+map("n", "su", "<cmd>FzfLua lsp_references<cr>", { desc = "[G]oto [R]eferences" })
+map("n", "fo", "<cmd>FzfLua lsp_document_symbols<cr>", { desc = "[D]ocument [S]ymbols" })
+map("n", "<leader>fo", "<cmd>FzfLua lsp_live_workspace_symbols<cr>", { desc = "[W]orkspace [S]ymbols" })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', 'gE', vim.diagnostic.goto_prev)
-vim.keymap.set('n', 'ge', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set("n", "gE", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "ge", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 
 map("n", "<leader>bk", function()
   Snacks.bufdelete()
 end, { desc = "Delete Buffer" })
-
-
---
+++ nvim/lua/plugins/fzf.lua
@@ -0,0 +1,11 @@
+return {
+  "ibhagwan/fzf-lua",
+  keys = {
+    -- disable the keymap to grep files
+    { "<leader>/", false },
+    { "<leader><space>", false },
+    { "<leader>?", false },
+    { "<leader>,", false },
+    { "<leader>.", false },
+  },
+}
